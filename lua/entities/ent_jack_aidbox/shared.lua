b656def70b1e7ad43b99ad6e9f80dfaeed5eb961
Redox <mail@redox.to>
2022-09-19 17:41:16 +0200

    Delete aidbox

--- lua/entities/ent_jack_aidbox/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type 			= "anim"
-ENT.PrintName		= "J.I. Aid Package"
-ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
-ENT.Information         = "BLOOIE-SCHLANG"
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
