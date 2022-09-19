d5992879320dac4df1be230615c74b76a400fbdf
Redox <mail@redox.to>
2022-09-19 18:48:35 +0200

    Delete sentry laptop

--- lua/entities/ent_jack_terminal/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type 			= "anim"
-ENT.PrintName		= "Sentry Terminal"
-ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
-ENT.Information         = "BLOOIE-SCHLANG"
-ENT.Spawnable			= true
-ENT.AdminSpawnable		= true
