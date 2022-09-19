94bb9d5e0db27f500c0f23415ac26ebc9135e490
Redox <mail@redox.to>
2022-09-19 19:15:53 +0200

    Add removed function

--- lua/autorun/server/ji_ds_server.lua
+++ lua/autorun/server/ji_ds_server.lua
@@ -6,3 +6,11 @@ local function CmdDet( ... )
 end
 concommand.Add( "jacky_remote_det", CmdDet )
 
+function JackaGenericUseEffect( ply )
+    if ply:IsPlayer() then
+        local Wep = ply:GetActiveWeapon()
+        if IsValid( Wep ) then Wep:SendWeaponAnim( ACT_VM_DRAW ) end
+        ply:ViewPunch( Angle( 1, 0, 0 ) )
+        ply:SetAnimation( PLAYER_ATTACK1 )
+    end
+end
