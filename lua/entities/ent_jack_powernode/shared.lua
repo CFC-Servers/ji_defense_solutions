3d01ee32a818270232f6db9a9ca28ea46bfee7d8
Redox <mail@redox.to>
2022-09-20 18:19:30 +0200

    Remove useless information

--- lua/entities/ent_jack_powernode/shared.lua
+++ lua/entities/ent_jack_powernode/shared.lua
@@ -2,6 +2,6 @@ ENT.Type 			= "anim"
 ENT.PrintName		= "Electrical Node"
 ENT.Author			= "Jackarunda"
 ENT.Category			= "J.I. Defense Solutions"
-ENT.Information         = "BLOOIE-SCHLANG"
+
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
