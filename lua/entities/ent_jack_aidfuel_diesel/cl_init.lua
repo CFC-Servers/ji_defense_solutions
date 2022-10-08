b79917419171a61bd2a15d2c936cda54c0161b56
Redox <mail@redox.to>
2022-10-08 15:06:31 +0200

    Remove old code

--- lua/entities/ent_jack_aidfuel_diesel/cl_init.lua
+++ lua/entities/ent_jack_aidfuel_diesel/cl_init.lua
@@ -10,11 +10,6 @@ function ENT:Initialize()
     Matricks:Scale( Vector( 1, .9, .65 ) )
     self.Prettiness:EnableMatrix( "RenderMultiply", Matricks )
     self.Prettiness:SetMaterial( "models/mat_jack_aidfuel_diesel" )
-    self.Thing = ClientsideModel( "models/weapons/w_c4.mdl" )
-    self.Thing:SetPos( self:GetPos() )
-    self.Thing:SetParent( self )
-    self.Thing:SetNoDraw( true )
-    self.Thing:SetModelScale( .7, 0 )
 end
 
 function ENT:Draw()
@@ -22,12 +17,6 @@ function ENT:Draw()
     local Ang = self:GetAngles()
     Ang:RotateAroundAxis( Ang:Up(), 90 )
     self.Prettiness:SetRenderAngles( Ang )
-
-    if self:GetDTBool( 0 ) then
-        self.Thing:SetRenderOrigin( self:GetPos() + self:GetUp() * 6 + self:GetForward() * 4 )
-        self.Thing:SetRenderAngles( Ang )
-        self.Thing:DrawModel()
-    end
 
     self.Prettiness:DrawModel()
 end
