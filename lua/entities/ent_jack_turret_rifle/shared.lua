b420fba04d043c8a3d76d5c4940c62caf698d6dd
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2023-02-15 14:20:43 -0700

    Prune long range turret selection

--- lua/entities/ent_jack_turret_rifle/shared.lua
+++ lua/entities/ent_jack_turret_rifle/shared.lua
@@ -2,6 +2,6 @@ ENT.Type = "anim"
 ENT.PrintName = "Rifle ST"
 ENT.Author = "Jackarunda"
 ENT.Category = "J.I. Defense Solutions"
-ENT.Spawnable = true
+ENT.Spawnable = false
 
 ENT.Base = "ent_jack_turret_base"
