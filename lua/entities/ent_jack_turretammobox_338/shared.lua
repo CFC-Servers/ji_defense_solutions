8658d0b8be83d6ee5fa07620098c4f4f660015e2
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2023-02-16 13:37:59 -0700

    Make 338 not spawnable for real

--- lua/entities/ent_jack_turretammobox_338/shared.lua
+++ lua/entities/ent_jack_turretammobox_338/shared.lua
@@ -1,8 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= ".338LM AmmoBox"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category		= "J.I. Defense Solutions"
+ENT.Spawnable       = false
 
-ENT.Spawnable			= true
-
-ENT.Base="ent_jack_turretammobox_base"
+ENT.Base = "ent_jack_turretammobox_base"
