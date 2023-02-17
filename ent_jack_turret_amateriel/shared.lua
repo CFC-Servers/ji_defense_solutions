214400274f3b81ca43fb7f6de858012de0bb0a04
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2023-02-17 12:11:29 -0700

    Revert "Add idle drain multipler"
    This reverts commit 7b3b564f2035fbe74d47191c47164d34894d9d57.

--- ent_jack_turret_amateriel/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type = "anim"
-ENT.PrintName = "Anti-Materiel ST"
-ENT.Author = "Jackarunda"
-ENT.Category = "J.I. Defense Solutions"
-ENT.Spawnable = false
-
-ENT.Base = "ent_jack_turret_base"
--- ent_jack_turret_base/shared.lua
+++
@@ -1,6 +0,0 @@
-ENT.Type = "anim"
-ENT.PrintName = "Sentry Turret"
-ENT.Author = "Jackarunda"
-ENT.Category = "JI Defense Solutions"
-ENT.Spawnable = false
-ENT.AdminSpawnable = false
